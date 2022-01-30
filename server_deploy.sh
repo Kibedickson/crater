66b60383ffbc40dc67877eb251e04f673de59c96
Dickson <kibedickson3@gmail.com>
2022-01-30 18:35:54 +0300

    Production Workflow

---
+++ server_deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+set -e
+
+echo " Deploying Application... "
+
+# Enter Maintenance Mode
+(docker-compose exec app php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
+  # update Codebase
+  git fetch origin deploy
+  git reset --hard origin/deploy
+
+  docker-compose down
+  docker-compose up -d
+
+  # Install dependencies based on composer.lock file
+  docker-compose exec app composer install --no-interaction --prefer-dist --optimize-autoloader
+
+  # Migrate database
+  docker-compose exec app php artisan migrate --force
+
+  # Stop Cronjob
+
+  # Clear Cache
+  docker-compose exec app php artisan config:clear
+  docker-compose exec app php artisan cache:clear
+
+# Exit Maintenance Mode
+docker-compose exec app php artisan up
+
+
+echo " Application Deployed ! "
