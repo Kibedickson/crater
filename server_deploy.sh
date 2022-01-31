5bd3f55d78a324dfe1c3b2a61b2399d260a24fee
Dickson <kibedickson3@gmail.com>
2022-01-31 12:36:12 +0300

    Build for Prod

--- server_deploy.sh
+++ server_deploy.sh
@@ -4,27 +4,32 @@ set -e
 echo " Deploying Application... "
 
 # Enter Maintenance Mode
+echo "docker-compose exec app php artisan down"
 (docker-compose exec app php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
-  # update Codebase
-  git fetch origin deploy
-  git reset --hard origin/deploy
+# update Codebase
+git fetch origin deploy
+git reset --hard origin/deploy
 
-  docker-compose up -d --build
+echo "docker-compose up -d --build"
+docker-compose up -d --build
 
-  # Install dependencies based on composer.lock file
-  docker-compose exec app composer install --no-interaction --prefer-dist --optimize-autoloader --ignore-platform-reqs
+# Install dependencies based on composer.lock file
+echo "docker-compose exec app composer install --no-interaction --prefer-dist --optimize-autoloader --ignore-platform-reqs"
+docker-compose exec app composer install --no-interaction --prefer-dist --optimize-autoloader --ignore-platform-reqs
 
-  # Migrate database
-  docker-compose exec app php artisan migrate --force
+# Migrate database
+echo "docker-compose exec app php artisan migrate --force"
+docker-compose exec app php artisan migrate --force
 
-  # Stop Cronjob
+# Stop Cronjob
 
-  # Clear Cache
-  docker-compose exec app php artisan config:clear
-  docker-compose exec app php artisan cache:clear
+# Clear Cache
+echo "docker-compose exec app php artisan config:clear"
+docker-compose exec app php artisan config:clear
+docker-compose exec app php artisan cache:clear
 
 # Exit Maintenance Mode
+echo "docker-compose exec app php artisan up"
 docker-compose exec app php artisan up
-
 
 echo " Application Deployed ! "
